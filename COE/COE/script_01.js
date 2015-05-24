5516a1cda788f348152ff60e2cb8af89c7cf59f7
Tanner Kranich <tkranich1@gmail.com>
2015-05-23 23:24:53 -0400

    First commit, entire VS project folder uploaded

---
+++ COE/COE/script_01.js
@@ -0,0 +1,24 @@
+﻿function mapClick() {
+    //Need to update map when clicked to make visible changes by other players
+    document.getElementById("mapCol").innerHTML = "MAP";
+}
+
+function landsClick() {
+    document.getElementById("mapCol").innerHTML = "LANDS_DATA";
+}
+
+function popClick() {
+    document.getElementById("mapCol").innerHTML = "POPULATION_DATA";
+}
+
+function forrelClick() {
+    document.getElementById("mapCol").innerHTML = "FOREIGN_RELATIONS_DATA";
+}
+
+function govClick() {
+    document.getElementById("mapCol").innerHTML = "GOVERNMENT_DATA";
+}
+
+function infraClick() {
+    document.getElementById("mapCol").innerHTML = "INFRASTRUCTURE_DATA";
+}
