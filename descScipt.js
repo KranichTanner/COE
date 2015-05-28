bd30c2544ae38bb77741001424a2738dbac7549a
Tanner Kranich <tkranich1@gmail.com>
2015-05-28 15:56:46 -0400

    Forgot to put new js file into git folder.

---
+++ descScipt.js
@@ -0,0 +1,12 @@
+﻿window.onload = function(){
+    setComponentDim(1024, 850, 850);
+}
+
+function setComponentDim(bodyw, mapw, maph) {
+    document.getElementById("divBody").setAttribute("style", "width:" + bodyw + "px");
+    document.getElementById("contentArea").setAttribute("style", "height:" + maph + "px");
+    document.getElementById("navBar").setAttribute("style", "width:" + (bodyw - mapw) + "px");
+    document.getElementById("mapBar").setAttribute("style", "width:" + mapw + "px");
+    document.getElementById("canvasMap").width = mapw;
+    document.getElementById("canvasMap").height = maph;
+}
