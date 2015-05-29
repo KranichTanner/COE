a33e98544f01acca639a1dbb09a5a047088fd0b1
Tanner Kranich <tkranich1@gmail.com>
2015-05-29 14:51:23 -0400

    Applied a fix to repainting map. May need revision.

--- script_01.js
+++ script_01.js
@@ -1,18 +1,13 @@
 ﻿window.onload = function () {
-    setComponentDim(1024, 850, 850); //A simple function to easily resize components during development
-    for (y = 0; y < 17; y++){
-        for(x = 0; x < 17; x++){
-            paintMap(x * 50, y * 50);
-        }
-    }
+    mapClick();
 };
 
-function paintMap(x, y) {
+function paintMapSpot(x, y) {
     var ctx = document.getElementById("canvasMap").getContext("2d");
     var mapPiece = new Image();
     mapPiece.onload = function () {
 
-        ctx.drawImage(mapPiece, x, y);//Produces a warped image on screen, possibly due to percents in formatting
+        ctx.drawImage(mapPiece, x, y);
     };
     mapPiece.src = "SampleMapSpot.png";//A 50px by 50px png image
 }
@@ -24,4 +19,15 @@ function setComponentDim(bodyw, mapw, maph) {
     document.getElementById("mapBar").setAttribute("style", "width:" + mapw + "px");
     document.getElementById("canvasMap").width = mapw;
     document.getElementById("canvasMap").height = maph;
+}
+
+function mapClick() {
+
+    setComponentDim(1024, 850, 850); //A simple function to easily resize components during development
+
+    for (y = 0; y < 17; y++) {
+        for (x = 0; x < 17; x++) {
+            paintMapSpot(x * 50, y * 50);
+        }
+    }
 }
