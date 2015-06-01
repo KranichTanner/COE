dfec43e4aaff62b35e7fba36d4af8f88bfbd53a5
Daniel Kerrigan <dkerriga@nd.edu>
2015-05-31 21:40:43 -0400

    Added call to mapClick again, cleaned up some JS

--- script_01.js
+++ script_01.js
@@ -2,27 +2,30 @@
     mapClick();
 };
 
+// function to getElementById
+function getE(id){
+  return document.getElementById(id);
+}
+
 function paintMapSpot(x, y) {
-    var ctx = document.getElementById("canvasMap").getContext("2d");
+    var ctx = getE("canvasMap").getContext("2d");
     var mapPiece = new Image();
     mapPiece.onload = function () {
-
         ctx.drawImage(mapPiece, x, y);
     };
     mapPiece.src = "SampleMapSpot.png";//A 50px by 50px png image
 }
 
 function setComponentDim(bodyw, mapw, maph) {
-    document.getElementById("divBody").setAttribute("style", "width:" + bodyw + "px");
-    document.getElementById("contentArea").setAttribute("style", "height:" + maph + "px");
-    document.getElementById("navList").setAttribute("style", "width:" + (bodyw - mapw) + "px");
-    document.getElementById("mapBar").setAttribute("style", "width:" + mapw + "px");
-    document.getElementById("canvasMap").width = mapw;
-    document.getElementById("canvasMap").height = maph;
+    getE("divBody").setAttribute("style", "width:" + bodyw + "px");
+    getE("contentArea").setAttribute("style", "height:" + maph + "px");
+    getE("navList").setAttribute("style", "width:" + (bodyw - mapw) + "px");
+    getE("mapBar").setAttribute("style", "width:" + mapw + "px");
+    getE("canvasMap").width = mapw;
+    getE("canvasMap").height = maph;
 }
 
 function mapClick() {
-
     setComponentDim(1024, 850, 850); //A simple function to easily resize components during development
 
     for (y = 0; y < 17; y++) {
