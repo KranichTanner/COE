1a851667d978f4bfb0fa043e770e4a4f3827dbbb
Tanner Kranich <tkranich1@gmail.com>
2015-06-02 22:11:05 -0400

    Did a lot to map. Removed and commented some js effecting the map, may keep some. The map now will be img that cover the portion of the page. This will be possible to expand to entire page if img tags can be made in js and easily manipulated. Changed jquery library source, don't think that changed anything, to allow for dragging capability. Added dragging to map in form of dragging a div.

--- script_01.js
+++ script_01.js
@@ -1,5 +1,11 @@
 ﻿window.onload = function () {
-    mapClick();
+    //mapClick();
+    setComponentDim(1024, 850, 850);
+    //Not sure quite yet how to add img tags dynamically for an "infinite" sized display. This will be for the 850px display as it is.
+    getE("panelA").setAttribute("style", "top:0; left:0;");
+    getE("panelB").setAttribute("style", "top:0; left:850;");
+    getE("panelC").setAttribute("style", "top:850; left:0;");
+    getE("panelD").setAttribute("style", "top:850; left:850;");
 };
 
 // function to getElementById
@@ -7,14 +13,16 @@ function getE(id){
   return document.getElementById(id);
 }
 
-function paintMapSpot(x, y, img) {
+/*function paintMapSpot(x, y, img) {
     var ctx = getE("canvasMap").getContext("2d");
+    ctx.beginPath();
     var mapPiece = new Image();
     mapPiece.onload = function () {
         ctx.drawImage(mapPiece, x, y);
     };
     mapPiece.src = img;//A 50px by 50px png image
-}
+    ctx.closePath();
+}*/
 
 //A simple function to easily resize components during development
 function setComponentDim(bodyw, mapw, maph) {
@@ -22,11 +30,9 @@ function setComponentDim(bodyw, mapw, maph) {
     getE("contentArea").setAttribute("style", "height:" + maph + "px");
     getE("navList").setAttribute("style", "width:" + (bodyw - mapw) + "px");
     getE("mapBar").setAttribute("style", "width:" + mapw + "px");
-    getE("canvasMap").width = mapw;
-    getE("canvasMap").height = maph;
 }
 
-function mapClick() {
+/*function mapClick() {
     setComponentDim(1024, 850, 850);
 
     for (y = 0; y < 17; y++) {
@@ -39,4 +45,6 @@ function mapClick() {
             paintMapSpot(x * 50, y * 50, "SampleMapSpot2.png");
         }
     }
-}
+
+    //Replace with test clause to gen a new map section. Gen it. Merge section images into one. Save that image. Display needed pieces.
+}*/
