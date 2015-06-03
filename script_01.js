fa46146c8755954649e2f00f1736f247b02f9fe5
Tanner Kranich <tkranich1@gmail.com>
2015-06-03 00:50:47 -0400

    Made so image dragged is only seen within the div. Formatting is strange and still working on fixing it. Position attributes are lost when go to map is clicked, js isn't being read, needs to be fixed.

--- script_01.js
+++ script_01.js
@@ -1,11 +1,6 @@
 ﻿window.onload = function () {
     //mapClick();
     setComponentDim(1024, 850, 850);
-    //Not sure quite yet how to add img tags dynamically for an "infinite" sized display. This will be for the 850px display as it is.
-    getE("panelA").setAttribute("style", "top:0; left:0;");
-    getE("panelB").setAttribute("style", "top:0; left:850;");
-    getE("panelC").setAttribute("style", "top:850; left:0;");
-    getE("panelD").setAttribute("style", "top:850; left:850;");
 };
 
 // function to getElementById
@@ -30,6 +25,11 @@ function setComponentDim(bodyw, mapw, maph) {
     getE("contentArea").setAttribute("style", "height:" + maph + "px");
     getE("navList").setAttribute("style", "width:" + (bodyw - mapw) + "px");
     getE("mapBar").setAttribute("style", "width:" + mapw + "px");
+    //Not sure quite yet how to add img tags dynamically for an "infinite" sized display. This will be for the 850px display as it is.
+    getE("panelA").setAttribute("style", "top:0px; left:0px;");
+    getE("panelB").setAttribute("style", "top:0px; left:" + mapw + "px;");
+    getE("panelC").setAttribute("style", "top:" + maph + "px; left:0px;");
+    getE("panelD").setAttribute("style", "top:" + maph + "px; left:" + mapw + "px;");
 }
 
 /*function mapClick() {
