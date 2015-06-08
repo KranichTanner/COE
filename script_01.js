c2a8a1781b6dc003de4ffb3e7df670c03bdc199f
Tanner Kranich <tkranich1@gmail.com>
2015-06-08 18:51:34 -0400

    Made so clicking on a panel will generate an image in the canvas and set the src of that panel to what the canvas generated. Panels with images already added ignores the method to save resources.

--- script_01.js
+++ script_01.js
@@ -17,38 +17,26 @@ function getE(id){
   return document.getElementById(id);
 }
 
-/*function paintMapSpot(x, y, img) {
-    var ctx = getE("canvasMap").getContext("2d");
-    ctx.beginPath();
-    var mapPiece = new Image();
-    mapPiece.onload = function () {
-        ctx.drawImage(mapPiece, x, y);
-    };
-    mapPiece.src = img;//A 50px by 50px png image
-    ctx.closePath();
-}*/
+function genPanel(panelid) {
+    if (getE(panelid).src == "") {
 
-//A simple function to easily resize components during development
-function setComponentDim(bodyw, mapw, maph) {
-    getE("divBody").setAttribute("style", "width:" + bodyw + "px");
-    getE("contentArea").setAttribute("style", "height:" + maph + "px");
-    getE("navList").setAttribute("style", "width:" + (bodyw - mapw) + "px");
-    getE("mapBar").setAttribute("style", "width:" + mapw + "px");
+        var canvas = getE("canvasMap");
+        var ctx = canvas.getContext("2d");
+
+        var mapPiece = new Image();
+        mapPiece.src = "SampleMapSpot.png";
+        var mapPiece2 = new Image();
+        mapPiece2.src = "SampleMapSpot2.png";
+
+        for (y = 0; y <= 850; y += 50) {
+            for (x = 0; x <= 850; x += 50) {
+                ctx.drawImage(mapPiece2, x, y);
+            }
+        }
+
+        $("#" + panelid).attr("src", canvas.toDataURL("image/png"));
+    }
+
 }
 
-/*function mapClick() {
-    setComponentDim(1024, 850, 850);
 
-    for (y = 0; y < 17; y++) {
-        for (x = 0; x < 17; x++) {
-            paintMapSpot(x * 50, y * 50, "SampleMapSpot.png");
-        }
-    }
-    for (y = 850; y < 17; y++) {
-        for (x = 850; x < 17; x++) {
-            paintMapSpot(x * 50, y * 50, "SampleMapSpot2.png");
-        }
-    }
-
-    //Replace with test clause to gen a new map section. Gen it. Merge section images into one. Save that image. Display needed pieces.
-}*/
