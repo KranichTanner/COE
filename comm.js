8a4f4dd127afd6d9f6594625a9643e5979b3bc47
Tanner Kranich <tkranich1@gmail.com>
2015-07-07 17:31:55 -0400

    Added a log in procedure

--- comm.js
+++ comm.js
@@ -3,3 +3,38 @@
 $(document).ready(function () {
     socket = io.connect("http://localhost:3000");
 });
+
+function logIn(user, pass) {
+    socket.emit("loginUsername", user);
+
+    socket.on("loginPassword", function (data) {
+        if (data === true) {
+            socket.emit("loginPassword", pass);
+        }
+        else {
+            socket.emit("loginFail");
+        }
+    });
+
+    socket.on("loginSuccess", function (data) {
+        if (data === true) {
+            socket.emit("loginSuccess");
+            $("body").empty();
+            $("body").load("game.html");
+        }
+        else {
+            socket.emit("loginFail");
+        }
+    });
+
+    socket.on("loginFail", function () {
+    });
+}
+
+function landClick() {
+    socket.emit("landAllGovData");
+
+    socket.on("landAllGovData", function (data) {
+        document.getElementById("landAllGovData").innerHTML = data;
+    });
+}
--- server.js
+++ server.js
@@ -2,8 +2,35 @@
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
-io.sockets.on("connection", function (socket) {
+io.on("connection", function (socket) {
     console.log("User has connected");
+
+    socket.on("disconnect", function (socket) {
+        console.log("User has disconnected.");
+    });
+    
+    socket.on("loginUsername", function (data) {
+        socket.emit("loginPassword", true);
+    });
+    
+    socket.on("loginPassword", function (data) { 
+        socket.emit("loginSuccess", true);
+    });
+    
+    socket.on("loginSuccess", function () {
+        console.log("Login Successful!")
+    });
+    
+    socket.on("loginFail", function () { 
+        console.log("Login Fail!");
+        socket.emit("loginFail");
+    });
+
+    socket.on("landAllGovData", function () {
+        console.log("Land button clicked...");
+        io.emit("landAllGovData", "Democracy");
+    });
+
 });
 
 http.listen(3000, function () {
